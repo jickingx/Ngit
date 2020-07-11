ce23a351b5a061290d312111fe2871bd42942f4d
jickingx <jickingx@gmail.com>
2020-07-11 08:59:14 +0800

    update enemy max speed

--- src/EnemyManager.gd
+++ src/EnemyManager.gd
@@ -5,7 +5,7 @@ const ENEMY00: PackedScene = preload("res://src/actors/Enemy00.tscn")
 var enemy_count:= 0
 var enemy_speed_base:= 256.0
 var enemy_speed_buff:= 0.0
-var enemy_speed_max := 768.0
+var enemy_speed_max := 500.0
 
 
 func spawn_start() -> void:
@@ -26,9 +26,9 @@ func _on_Timer_timeout():
 	Global.current_scene.add_child(enemy)
 	
 	#update buff
-	enemy_speed_buff += ((enemy_speed_base + enemy_speed_buff) / 8) / 2 
+	enemy_speed_buff += ((enemy_speed_base + enemy_speed_buff) / 4) / 2 
 	enemy_speed_buff = clamp(enemy_speed_buff, 0, enemy_speed_max)
-	
+	#print_debug(enemy_speed_buff)
 	if enemy_count >= 3 :
 		enemy_count = 0
 		$Timer.wait_time -= .2
